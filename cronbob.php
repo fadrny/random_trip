64b9c9c2c0aa76f9e9019b6fb4951a033db9ffde
marekParek <48125221+fadrny@users.noreply.github.com>
2021-09-28 22:06:03 +0200

    edit cache control: no cache

--- cronbob.php
+++ cronbob.php
@@ -5,6 +5,10 @@ require __DIR__ . '/func.php';
 $database = new db;
 $f = new func;
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 if($database->add_new_lottery_record($f->is_it_today(), $database->get_random_place()['placeName']))
     echo "new record written \r\n";
 else
